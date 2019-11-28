a736f7d31ff30e551b80dafaa444dbc4d7a73522
SergiuDeveloper <sergiudev@hotmail.com>
2019-11-28 21:29:51 +0200

    Implemented client IPs whitelist

--- Server/Headers/SpecializedServer.hpp
+++ Server/Headers/SpecializedServer.hpp
@@ -2,17 +2,23 @@
 
 #define SUCCESS_MYSQL_DB_CONNECTED                                                      "Successfully connected to MySQL database!"
 #define SUCCESS_CLIENT_CONNECTED(clientIP)                                              ((string)"Client " + clientIP + " successfully connected!")
-#define ERROR_MYSQL_DB_CONNECTION                                                       "Failed to connect to MySQL database! Check your credentials!"
-#define PROMPT_MYSQL_DB_CREDENTIALS                                                     "Please enter the password for the MySQL database superuser(root)"
+#define ERROR_CLIENT_NOT_WHITELISTED(clientIP)                                          ((string)"Could not connect client " + clientIP + " to the server, as he is not whitelisted!")
+#define ERROR_MYSQL_GENERIC_ERROR(mySQLErrorCode, mySQLExceptionMessage)                ((string)"Failed to connect to MySQL database! Runtime error " + to_string(mySQLErrorCode) + ": \"" + mySQLExceptionMessage + "\"")
+#define PROMPT_MYSQL_DB_CREDENTIALS                                                     "Please enter the password for the MySQL database superuser(root): "
 #define MYSQL_SERVER_PROTOCOL                                                           "tcp"
 #define MYSQL_SERVER_IP                                                                 "localhost"
 #define MYSQL_SERVER_PORT                                                               3306
 #define MYSQL_SERVER_SUPERUSER                                                          "root"
+#define MYSQL_DATABASE_DEFAULT_SCHEMA                                                   "sys"
 #define MYSQL_CONNECTION_STRING(mySQLServerProtocol, mySQLServerIP, mySQLServerPort)    ((string)mySQLServerProtocol + "://" + mySQLServerIP + ":" + to_string(mySQLServerPort))
+#define MYSQL_GET_WHITELISTED_IP_COUNT_QUERY(clientIP)                                  ((string)"SELECT COUNT(*) FROM ClientIPsWhitelist WHERE ClientIP = \"" + clientIP + "\";")
 
 #include <mysql_connection.h>
 #include <cppconn/driver.h>
-#include <ncurses.h>
+#include <cppconn/statement.h>
+#include <cppconn/resultset.h>
+#include <cppconn/exception.h>
+#include <pthread.h>
 #include <iostream>
 #include <string>
 #include "Server.hpp"
--- Server/Sources/SpecializedServer.cpp
+++ Server/Sources/SpecializedServer.cpp
@@ -5,16 +5,23 @@ pthread_mutex_t SpecializedServer::mySQLConnectionMutex;
 
 SuccessState SpecializedServer::Start(unsigned int serverPort)
 {
-    string mySQLSuperuserPassword;
-    cout<<PROMPT_MYSQL_DB_CREDENTIALS<<'\n';
-    noecho();
-    getline(cin, mySQLSuperuserPassword);
-    echo();
+    string mySQLSuperuserPassword = getpass(PROMPT_MYSQL_DB_CREDENTIALS);
 
     pthread_mutex_init(&SpecializedServer::mySQLConnectionMutex, nullptr);
 
-    Driver * mySQLDriver = get_driver_instance();
-    SpecializedServer::mySQLConnection = mySQLDriver->connect(MYSQL_CONNECTION_STRING(MYSQL_SERVER_PROTOCOL, MYSQL_SERVER_IP, MYSQL_SERVER_PORT), MYSQL_SERVER_SUPERUSER, mySQLSuperuserPassword);
+    try
+    {
+        Driver * mySQLDriver = get_driver_instance();
+        SpecializedServer::mySQLConnection = mySQLDriver->connect(MYSQL_CONNECTION_STRING(MYSQL_SERVER_PROTOCOL, MYSQL_SERVER_IP, MYSQL_SERVER_PORT), MYSQL_SERVER_SUPERUSER, mySQLSuperuserPassword);
+        SpecializedServer::mySQLConnection->setSchema(MYSQL_DATABASE_DEFAULT_SCHEMA);
+    }
+    catch (SQLException & mySQLException)
+    {
+        delete SpecializedServer::mySQLConnection;
+        return SuccessState(false, ERROR_MYSQL_GENERIC_ERROR(mySQLException.getErrorCode(), mySQLException.what()));
+    }
+
+    cout<<SUCCESS_MYSQL_DB_CONNECTED<<'\n';
 
     return Server::Start(serverPort, SpecializedServer::ClientConnected_EventCallback);
 }
@@ -26,7 +33,8 @@ SuccessState SpecializedServer::Stop()
         return successState;
 
     while (!pthread_mutex_trylock(&SpecializedServer::mySQLConnectionMutex));
-    delete SpecializedServer::mySQLConnection;
+    if (SpecializedServer::mySQLConnection != nullptr)
+        delete SpecializedServer::mySQLConnection;
     pthread_mutex_unlock(&SpecializedServer::mySQLConnectionMutex);
 
     pthread_mutex_destroy(&SpecializedServer::mySQLConnectionMutex);
@@ -36,5 +44,49 @@ SuccessState SpecializedServer::Stop()
 
 void SpecializedServer::ClientConnected_EventCallback(ClientSocket clientSocket)
 {
+    Statement * mySQLStatement;
+    ResultSet * mySQLResultSet;
+    bool isWhitelistedIP = false;
+
+    try
+    {
+        mySQLStatement = SpecializedServer::mySQLConnection->createStatement();
+        mySQLResultSet = mySQLStatement->executeQuery(MYSQL_GET_WHITELISTED_IP_COUNT_QUERY(clientSocket.clientIP));
+
+        if (mySQLResultSet->next())
+            isWhitelistedIP = (mySQLResultSet->getInt(1) > 0);
+    }
+    catch (SQLException & mySQLException)
+    {
+        cout<<ERROR_MYSQL_GENERIC_ERROR(mySQLException.getErrorCode(), mySQLException.what())<<'\n';
+
+        if (mySQLStatement != nullptr)
+            delete mySQLStatement;
+        if (mySQLResultSet != nullptr)
+            delete mySQLResultSet;
+    }
+
+    if (mySQLStatement != nullptr)
+        delete mySQLStatement;
+    if (mySQLResultSet != nullptr)
+        delete mySQLResultSet;
+
+    if (!isWhitelistedIP)
+    {
+        for (size_t clientSocketsIterator = 0; clientSocketsIterator < SpecializedServer::clientSockets.size(); ++clientSocketsIterator)
+            if (SpecializedServer::clientSockets[clientSocketsIterator].clientIP == clientSocket.clientIP)
+            {
+                while (!pthread_mutex_trylock(&SpecializedServer::clientSocketsMutex));
+                close(clientSocket.clientSocketDescriptor);
+                
+                SpecializedServer::clientSockets[clientSocketsIterator] = SpecializedServer::clientSockets[SpecializedServer::clientSockets.size() - 1];
+                SpecializedServer::clientSockets.pop_back();
+                pthread_mutex_unlock(&SpecializedServer::clientSocketsMutex);
+            }
+
+        cout<<ERROR_CLIENT_NOT_WHITELISTED(clientSocket.clientIP)<<'\n';
+        return;
+    }
+
     cout<<SUCCESS_CLIENT_CONNECTED(clientSocket.clientIP)<<'\n';
 }
